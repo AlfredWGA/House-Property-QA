722e16bd5c9a3cd1c2c4c34c3f87b2410159b1db
xuhaiyangsnow <xuhaiyangsnow@didiglobal.com>
2020-10-22 16:33:52 +0800

    更新了一些baseline的训练代码

--- src/baseline/baseline.py
+++ src/baseline/baseline.py
@@ -8,11 +8,11 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 from tqdm import tqdm
-import tensorflow as tf
-import tensorflow.keras.backend as K
+# import tensorflow as tf
+# import tensorflow.keras.backend as K
 import os
 # from transformers import *
-print(tf.__version__)
+# print(tf.__version__)
 from sklearn.metrics import roc_auc_score, f1_score
 from pathlib import Path
 import logging
@@ -34,27 +34,30 @@ device = torch.device("cuda:3")
 # null.tpl [markdown]
 # # 参数下载地址 https://huggingface.co/bert-base-chinese
 
+PROJECT_ROOT_PATH = (Path(__file__).parent.parent.parent).resolve()
+RAW_DATA_PATH = PROJECT_ROOT_PATH/'data/raw_data'
+
 # %%
-train_left = pd.read_csv('./train/train.query.tsv',sep='\t',header=None)
+train_left = pd.read_csv(RAW_DATA_PATH/'./train/train.query.tsv',sep='\t',header=None)
 train_left.columns=['id','q1']
-train_right = pd.read_csv('./train/train.reply.tsv',sep='\t',header=None)
+train_right = pd.read_csv(RAW_DATA_PATH/'./train/train.reply.tsv',sep='\t',header=None)
 train_right.columns=['id','id_sub','q2','label']
 df_train = train_left.merge(train_right, how='left')
 df_train['q2'] = df_train['q2'].fillna('好的')
-test_left = pd.read_csv('./test/test.query.tsv',sep='\t',header=None, encoding='gbk')
+test_left = pd.read_csv(RAW_DATA_PATH/'./test/test.query.tsv',sep='\t',header=None, encoding='gbk')
 test_left.columns = ['id','q1']
-test_right =  pd.read_csv('./test/test.reply.tsv',sep='\t',header=None, encoding='gbk')
+test_right =  pd.read_csv(RAW_DATA_PATH/'./test/test.reply.tsv',sep='\t',header=None, encoding='gbk')
 test_right.columns=['id','id_sub','q2']
 df_test = test_left.merge(test_right, how='left')
 
 
 # %%
-PATH = './'
-BERT_PATH = './'
-WEIGHT_PATH = './'
+# PATH = './'
+# BERT_PATH = './data/pretrain_model'
+# WEIGHT_PATH = './'
 MAX_SEQUENCE_LENGTH = 100
-CHECKPOINT_DIR = './ckpt'
-PRETRAIN_MODEL_DIR = './pretrain_model'
+# CHECKPOINT_PATH = PROJECT_ROOT_PATH'./ckpt'
+PRETRAIN_MODEL_PATH = PROJECT_ROOT_PATH/'./data/pretrain_model'
 input_categories = ['q1','q2']
 output_categories = 'label'
 
@@ -142,11 +145,11 @@ def search_f1(y_true, y_pred):
 
 # %%
 
-tokenizer = BertTokenizer.from_pretrained(BERT_PATH+'bert-base-chinese-vocab.txt')
+tokenizer = BertTokenizer.from_pretrained(PRETRAIN_MODEL_PATH/'bert-base-chinese'/'vocab.txt')
 
-inputs_path = Path('./train/inputs.npy')
-outputs_path = Path('./train/outputs.npy')
-test_inputs_path = Path('./test/test_inputs.npy')
+inputs_path = Path(RAW_DATA_PATH/'./train/inputs.npy')
+outputs_path = Path(RAW_DATA_PATH/'./train/outputs.npy')
+test_inputs_path = Path(RAW_DATA_PATH/'./test/test_inputs.npy')
 
 # 保存处理好的数据集
 if not outputs_path.exists():
@@ -172,7 +175,7 @@ else:
 class BertForHouseQA(nn.Module):
     def __init__(self):
         super(BertForHouseQA, self).__init__()
-        self.bert = BertModel.from_pretrained(os.path.join(PRETRAIN_MODEL_DIR, 'bert-base-chinese'), 
+        self.bert = BertModel.from_pretrained(os.path.join(PRETRAIN_MODEL_PATH, 'bert-base-chinese'), 
                     output_hidden_states=False, 
                     output_attentions=False)
                     
@@ -200,11 +203,11 @@ def train_pytorch(**kwargs):
     outputs = kwargs['outputs']
     test_inputs = kwargs['test_inputs']
     
-    gkf = GroupKFold(n_splits=5).split(X=df_train.q2, groups=df_train.id)
+    gkf = GroupKFold(n_splits=kwargs['n_splits']).split(X=df_train.q2, groups=df_train.id)
     
-
-
     oof = np.zeros((len(df_train),1))
+    test_preds = []
+
     for fold, (train_idx, valid_idx) in enumerate(gkf):
         logger.info(f'Fold No. {fold}')
         train_inputs = [inputs[i][train_idx] for i in range(len(inputs))]
@@ -212,11 +215,9 @@ def train_pytorch(**kwargs):
         valid_inputs = [inputs[i][valid_idx] for i in range(len(inputs))]
         valid_outputs = outputs[valid_idx]
 
-        valid_preds = []
-        test_preds = []
-
         train_set = HouseDataset(train_inputs, train_outputs)
         valid_set = HouseDataset(valid_inputs, valid_outputs)
+        test_set = HouseDataset(test_inputs, np.zeros_like(test_inputs[0])) # 测试集没有标签
 
         logger.info('Train set size: {}, valid set size {}'.format(
             len(train_set), len(valid_set)))
@@ -226,6 +227,9 @@ def train_pytorch(**kwargs):
                                 shuffle=True)
 
         valid_loader = DataLoader(valid_set,
+                                batch_size=512)
+
+        test_loader = DataLoader(test_set,
                                 batch_size=512)
 
         model = BertForHouseQA().cuda(device)
@@ -255,7 +259,7 @@ def train_pytorch(**kwargs):
             # =======================Training===========================
             # Set model to train mode.
             model.train()
-            steps = int(np.ceil(len(train_set) // kwargs['batch_size']))
+            steps = int(np.ceil(len(train_set) / kwargs['batch_size']))
             pbar = tqdm(desc='Epoch {}, loss {}'.format(epoch, 'NAN'),
                         total=steps)
             for i, sample in enumerate(train_loader):
@@ -305,24 +309,43 @@ def train_pytorch(**kwargs):
 
             # 保存每个验证折的预测值，用作最后整个训练集的f1评估
             oof[valid_idx] = valid_pred
-            # valid_preds.append(oof_p)
-            # test_preds.append(model.predict(test_inputs, batch_size=512))
-            valid_f1, _ = search_f1(valid_outputs, valid_pred)
+            valid_f1, _ = search_f1(valid_outputs, valid_pred)  # 寻找最佳分类阈值和f1 score
             print('Valid f1 score = ', valid_f1)
             # ==========================================================
+            # =======================Prediction========================
+            # Set model to evaluation mode.
+            model.eval()
+            with torch.no_grad():
+                # Prediction step
+                test_pred = []
+                steps = int(np.ceil(len(test_set) / 512))
+                for i, sample in tqdm(enumerate(test_loader), desc='Predicting', total=steps):
+                    y_true_local = sample[1].numpy()
+                    x, y_true = sample[0].cuda(
+                        device).long(), sample[1].cuda(device).float()
+
+                    model_outputs = model(x)
+                    # loss = criterion(model_outputs, y_true.unsqueeze(-1)).cpu().detach().item()
+                    # y_pred = outputs.argmax(dim=1).cpu().numpy()
+                    y_pred = model_outputs.cpu().detach().numpy()
+                    test_pred.append(y_pred)
+            test_pred = np.concatenate(test_pred, axis=0)
+            test_preds.append(test_pred)
             # ==========================================================
             # Save the model at the end of every epoch.
-            torch.save({
-                'model_name': 'BertForHouseQA',
-                'epoch': epoch,
-                'loss': loss,
-                'valid_f1': valid_f1,
-                'model_state_dict':  model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                # 'scheduler_state_dict': scheduler.state_dict()
-            }, f=os.path.join(CHECKPOINT_DIR, 'ckpt.pt'))
+            # torch.save({
+            #     'model_name': 'BertForHouseQA',
+            #     'epoch': epoch,
+            #     'loss': loss,
+            #     'valid_f1': valid_f1,
+            #     'model_state_dict':  model.state_dict(),
+            #     'optimizer_state_dict': optimizer.state_dict(),
+            #     # 'scheduler_state_dict': scheduler.state_dict()
+            # }, f=os.path.join(CHECKPOINT_PATH, 'ckpt.pt'))
 
-train_pytorch(batch_size=256, epoch=3, inputs=inputs, outputs=outputs, test_inputs=test_inputs)
+    return oof, test_preds
+
+oof, test_preds = train_pytorch(batch_size=128, epoch=5, n_splits=5, inputs=inputs, outputs=outputs, test_inputs=test_inputs)
 
 # %%
 # def create_model():
@@ -376,13 +399,13 @@ train_pytorch(batch_size=256, epoch=3, inputs=inputs, outputs=outputs, test_inpu
 
 
 # %%
-# 整个训练集的f1评估
+# 整个训练集的f1评估，寻找最佳的分类阈值
 best_score, best_t = search_f1(outputs, oof)
 
 
 # %%
 sub = np.average(test_preds, axis=0) 
-sub = sub > best_t
+sub = sub > best_t  # 用该分类阈值来输出测试集
 df_test['label'] = sub.astype(int)
 df_test[['id','id_sub','label']].to_csv('submission_beike_{}.csv'.format(best_score),index=False, header=None,sep='\t')
 
